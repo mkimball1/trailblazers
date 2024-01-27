6172aef06401461af071a7cbc556b50c72b15ea5
mkimball1 <lemortyjordan@gmail.com>
2024-01-26 22:57:07 -0800

    Works, but not catch invalid zipcode

--- src/APIs/prescriptiontrails.js
+++ src/APIs/prescriptiontrails.js
@@ -6,26 +6,22 @@ export const convertZipToLatLong = async (zipcode) => {
     let response = await fetch(api_address);
     response = await response.json()
 
-    return {latitude: response.location[3].latitude, longitude: response.location[3].longitude}
+    let latitude = response.location.find(obj => obj['country'] === 'US').latitude;
+    let longitude = response.location.find(obj => obj['country'] === 'US').longitude;
+    return {latitude: latitude, longitude: longitude} 
+    
 };
 
 // f255df9efb841bfe43ab2d741c695756ac4a6cf5
 
-export const createOptions = (zipcode) => {
-    let latitude = "";
-    let longitude = "";
-    let data = convertZipToLatLong(zipcode).then(my_data => {
-        console.log(my_data);
-        latitude = my_data.latitude
-        longitude = my_data.longitude;
-    })
-
+export const createOptions = (data) => {
+    console.log("Data: ", data)
     return {
         method: 'GET',
         url: 'https://trailapi-trailapi.p.rapidapi.com/trails/explore/',
         params: {
-            lat: latitude,
-            lon: longitude
+            lat: data.latitude,
+            lon: data.longitude
         },
         headers: {
             'X-RapidAPI-Key': '6aacd49bc6mshd729a53f4cd2021p1896ddjsn624706bd7e13',
@@ -34,13 +30,28 @@ export const createOptions = (zipcode) => {
     }
 };
 
-export const getResponse = async (zipcode) => {
+export const getResponse = async (data) => {
     try {
-        console.log("Success")
-        let response = await axios.request(createOptions(zipcode));
+        let response = await axios.request(createOptions({
+            "latitude": data.latitude, 
+            "longitude": data.longitude
+        }));
         response = response.data.data
         return response
     } catch (error) {
         console.error(error);
     }
+};
+
+const updateLatLong = async (zipcode) => {
+    let lat = "33.684566";
+    let long = "-117.826508";
+
+    const my_data = await convertZipToLatLong(zipcode);
+    lat = my_data.latitude;
+    long = my_data.longitude;
+
+    // Now lat and long are updated
+    console.log("hurray: ", lat, long);
+    return { lat, long };
 };
--- src/App.js
+++ src/App.js
@@ -2,33 +2,29 @@ import { useState,useEffect } from "react";
 import {getResponse, convertZipToLatLong} from "./APIs/prescriptiontrails"
 
 function App() {
-  const [zip, setZip] = useState("92697")
+  const [zip, setZip] = useState("94404")
   const [trailResults, setTrailResults] = useState({})
 
   useEffect(() => {
     // This code runs when `zip` changes
-    // console.log(zip)
     console.log(trailResults);
   }, [zip, trailResults]);
 
+  const submitZipcode = () => {
+    convertZipToLatLong(zip).then(data => {
+      let my_data = {
+        latitude: data.latitude,
+        longitude: data.longitude
+      }
+      getResponse(my_data).then(response => {
+          setTrailResults(response)
+      })
+  })
+  }
 
   return (
     <div>
-      <button onClick={() => {
-        getResponse(zip).then(response => {
-          setTrailResults(response)
-        })
-
-      //   convertZipToLatLong(92697).then((data) => {
-      //     console.log(`Lat: ${data.longitude}, Long: ${data.latitude}`)
-      // })
-
-        // convertZipToLatLong(92697)
-      }}> TEST!! CLICK ME </button>
-
-      {/* <button onClick={() => {
-        console.log(trailResults)
-      }}> CLICK ME 2</button> */}
+      <button onClick={submitZipcode}> SUBMIT </button>
 
       <h1> TEST, TEST@louie</h1>
     </div>
