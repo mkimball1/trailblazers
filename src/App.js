de7dbc6c21e4b03c1ce4e7610aef6bcf29dfeeff
MoustafaGhanemm <maghanem@uci.edu>
2024-01-27 16:24:40 -0800

    Created likes file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react";
 import UserInput from "./Components/UserInput";
-import SlideShow from "./Components/SlideShow.js";
+import SlideShow from "./Components/slideShow.js";
 
 //change 
 import initMap from "./APIs/googlemaps.js";
